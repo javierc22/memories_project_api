8c5498f63a13dd3c632656ddd8c0f8ede48c643d
javierc22 <javier.ec.22@gmail.com>
2021-08-25 23:55:27 -0400

    Delete post

--- controllers/posts.js
+++ controllers/posts.js
@@ -35,3 +35,11 @@ export const updatePost = async(req, res) => {
 
   res.status(200).json(updatedPost)
 }
+
+export const deletePost = async(req, res) => {
+  const { id } = req.params
+  if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).json({ message: `No post with id: ${id}`})
+
+  await PostMessage.findByIdAndRemove(id)
+  res.status(200).json({ message: "Post deleted successfully" })
+}
